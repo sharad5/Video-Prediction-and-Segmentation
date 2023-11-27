5f88d63de7ed2ed01ddf003b2da76b7d08993753
chaitali <cpjoshi202@gmail.com>
2023-11-27 11:54:24 -0500

    Dropout

--- unet_segmentation/main.py
+++ unet_segmentation/main.py
@@ -53,17 +53,17 @@ class SegmentationDataSet(Dataset):
 
 
 class encoding_block(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout):
+    def __init__(self, in_channels, out_channels, dropout_1,dropout_2):
         super(encoding_block, self).__init__()
         model = []
         model.append(nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False))
         model.append(nn.BatchNorm2d(out_channels))
         model.append(nn.ReLU(inplace=True))
-        model.append(nn.Dropout(dropout))
+        model.append(nn.Dropout(dropout_1))
         model.append(nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False))
         model.append(nn.BatchNorm2d(out_channels))
         model.append(nn.ReLU(inplace=True))
-        model.append(nn.Dropout(dropout))
+        model.append(nn.Dropout(dropout_2))
         self.conv = nn.Sequential(*model)
 
     def forward(self, x):
@@ -73,22 +73,24 @@ class encoding_block(nn.Module):
 class unet_model(nn.Module):
     def __init__(self, out_channels=49, features=[64, 128, 256, 512]):
         super(unet_model, self).__init__()
-        dropout=0
+        dropout_1=0.25
+        dropout_2=0.5
         self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        self.conv1 = encoding_block(3, features[0], dropout)
-        self.conv2 = encoding_block(features[0], features[1], dropout)
-        self.conv3 = encoding_block(features[1], features[2], dropout)
-        self.conv4 = encoding_block(features[2], features[3], dropout)
-        self.conv5 = encoding_block(features[3] * 2, features[3], dropout)
-        self.conv6 = encoding_block(features[3], features[2], dropout)
-        self.conv7 = encoding_block(features[2], features[1], dropout)
-        self.conv8 = encoding_block(features[1], features[0], 0)
+        self.conv1 = encoding_block(3, features[0], dropout_1, dropout_2)
+        self.conv2 = encoding_block(features[0], features[1], dropout_1,dropout_2)
+        self.conv3 = encoding_block(features[1], features[2], dropout_1,dropout_2)
+        self.conv4 = encoding_block(features[2], features[3], dropout_1,dropout_2)
+        self.conv5 = encoding_block(features[3] * 2, features[3], dropout_1,dropout_2)
+        self.conv6 = encoding_block(features[3], features[2], dropout_1,dropout_2)
+        self.conv7 = encoding_block(features[2], features[1], dropout_1,dropout_2)
+        self.conv8 = encoding_block(features[1], features[0], dropout_1,dropout_2)
         self.tconv1 = nn.ConvTranspose2d(features[-1] * 2, features[-1], kernel_size=2, stride=2)
         self.tconv2 = nn.ConvTranspose2d(features[-1], features[-2], kernel_size=2, stride=2)
         self.tconv3 = nn.ConvTranspose2d(features[-2], features[-3], kernel_size=2, stride=2)
         self.tconv4 = nn.ConvTranspose2d(features[-3], features[-4], kernel_size=2, stride=2)
-        self.bottleneck = encoding_block(features[3], features[3] * 2, 0)
+        self.bottleneck = encoding_block(features[3], features[3] * 2, 0, 0)
         self.final_layer = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         skip_connections = []
@@ -108,15 +110,19 @@ class unet_model(nn.Module):
         skip_connections = skip_connections[::-1]
         x = self.tconv1(x)
         x = torch.cat((skip_connections[0], x), dim=1)
+        x = self.dropout(x)
         x = self.conv5(x)
         x = self.tconv2(x)
         x = torch.cat((skip_connections[1], x), dim=1)
+        x = self.dropout(x)
         x = self.conv6(x)
         x = self.tconv3(x)
         x = torch.cat((skip_connections[2], x), dim=1)
+        x = self.dropout(x)
         x = self.conv7(x)
         x = self.tconv4(x)
         x = torch.cat((skip_connections[3], x), dim=1)
+        x = self.dropout(x)
         x = self.conv8(x)
         x = self.final_layer(x)
         return x
